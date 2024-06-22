66682333dd928aa9a95e00430cf3629593667f36
Akshat Bansal <akshatkba24@gmail.com>
2024-06-22 18:26:20 +0530

    setup of smtp using nodemailer done

--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@ const express = require('express');
 
 const { PORT } = require('./config/serverConfig');
 
+const { sendBasicEmail } = require('./services/email-service');
+
 const setupAndStartServer = ()=>{
     const app = express();
     app.use(express.json());
     app.use(express.urlencoded({extended:true}));
     app.listen(PORT, () => {
         console.log(`Listeninng on port: ${PORT}`);
+
+        const response = sendBasicEmail(
+            'support@admin.com',
+            'akshatkba24@gmail.com',
+            'This is a special testing email',
+            'Hey, how are you, I hope you like our app'
+        );
+
+        // console.log(response);
     })
 }
 
