b50163cbdbe8eb5a095a3624f3649cd5be4a9cf5
Akshat Bansal <akshatkba24@gmail.com>
2024-06-24 15:09:01 +0530

    added rabbitmq setup

--- src/index.js
+++ src/index.js
@@ -1,33 +1,29 @@
 const express = require('express');
 
 const { PORT } = require('./config/serverConfig');
-
-// const { sendBasicEmail } = require('./services/email-service');
+const { createChannel } = require('./utils/messageQueue')
 
 const jobs = require('./utils/job')
 const TicketController = require('./controllers/ticket-controller');
 
-const setupAndStartServer = ()=>{
+const m = require('./models/notificatioticket');
+const setupAndStartServer = async ()=>{
     const app = express();
     app.use(express.json());
     app.use(express.urlencoded({extended:true}));
+
 
     app.post('/api/v1/tickets', TicketController.create);
     
     app.listen(PORT, () => {
         console.log(`Listeninng on port: ${PORT}`);
-        jobs();
+        // jobs();
         // const response = sendBasicEmail(
         //     'support@admin.com',
         //     'akshatkba24@gmail.com',
         //     'This is a special testing email',
         //     'Hey, how are you, I hope you like our app'
         // );
-
-        // console.log(response);
-        // cron.schedule('* * * * *', () => {
-        //     console.log('running a task every minute');
-        // });
 
     })
 }
